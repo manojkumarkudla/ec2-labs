c52831cc3a1e70a4c0d52418964d0a1874e216cb
Manoj Kumar <manoj.kumar@cloudreach.com>
2022-07-21 13:16:21 +0530

    creating ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids =  [aws_security_group.ansible_server.id]
+  key_name = "talent-academy-key-pair"
+  subnet_id = data.aws_subnet.public.id
+
+
+  tags = {
+    Name = "ansible server"
+  }
+}
+
+resource "aws_eip" "ansible_server_id" {
+    instance = aws_instance.web.id
+    vpc = true
+  
+}
