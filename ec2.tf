97b74a6c5d2a02a16a659503dd5c17e96b21bf9e
Manoj Kumar <manoj.kumar@cloudreach.com>
2023-01-16 11:03:06 +0530

    changes

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids =  [aws_security_group.ansible_server.id]
   key_name = "talent-academy-key-pair"
   subnet_id = data.aws_subnet.public.id
 
 
   tags = {
-    Name = "ansible server"
+    Name = "ansible-server"
   }
 }
 
