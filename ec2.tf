656d2f7affaa3dfcc298305e324c5db5cfa1d84e
Manoj Kumar <manoj.kumar@cloudreach.com>
2023-05-15 11:55:10 +0530

    updat2e

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type =      "t2.micro"
-  vpc_security_group_ids =  [aws_security_group.ansible_server.id]
-  key_name =            "talent-academy-key-pair"
-  subnet_id = data.aws_subnet.public.id
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          =                      "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ansible_server.id]
+  key_name               = "talent-academy-key-pair"
+  subnet_id      = data.aws_subnet.public.id
 
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_eip" "ansible_server_id" {
-    instance = aws_instance.web.id
-    vpc = true
-  
+  instance = aws_instance.web.id
+  vpc      = true
+
 }
